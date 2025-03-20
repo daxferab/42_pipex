9d861988ee76eb6e65aae7ea4c05d5e461d8088d
daxferab <daxferab.19@gmail.com>
2025-03-20 01:33:36 +0100

    Shortening functions

--- src/execution.c
+++ src/execution.c
@@ -6,7 +6,7 @@
 /*   By: daxferna <daxferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 16:58:43 by daxferna          #+#    #+#             */
-/*   Updated: 2025/03/19 03:26:13 by daxferna         ###   ########.fr       */
+/*   Updated: 2025/03/20 01:33:00 by daxferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,8 @@
 static char	*get_path(char **envp, char	*cmd)
 {
 	char	*line;
-	char	**path_split;
+	char	**path_list;
 	char	*path;
-	char	*cmd2;
 	int		i;
 
 	if (access(cmd, X_OK) == 0)
@@ -26,18 +25,23 @@ static char	*get_path(char **envp, char	*cmd)
 	while (ft_strncmp(envp[i], "PATH=", 5))
 		i++;
 	line = ft_strdup(envp[i] + 5);
-	path_split = ft_split(line, ':');
-	cmd2 = ft_strjoin("/", cmd);
+	if (!line)
+		error(6);
+	path_list = ft_split(line, ':');
+	cmd = ft_strjoin("/", cmd);
+	if (!path_list || !cmd)
+		error(6);
 	i = 0;
-	while (path_split[i])
+	while (path_list[i])
 	{
-		path = ft_strjoin(path_split[i], cmd2);
+		path = ft_strjoin(path_list[i++], cmd);
+		if (!path)
+			error(6);
 		if (access(path, X_OK) == 0)
-			return (free_split(path_split), free(line), path);
+			return (free_split(path_list), free(line), path);
 		free(path);
-		i++;
 	}
-	return (free_split(path_split), free(line), free(cmd2), NULL);
+	return (free_split(path_list), free(line), free(cmd), NULL);
 }
 
 void	execute(char **envp, char *argv)
@@ -48,6 +52,8 @@ void	execute(char **envp, char *argv)
 	if (!ft_strncmp(argv, "", 1))
 		error(6);
 	cmd_list = ft_split(argv, ' ');
+	if (!cmd_list)
+		error(6);
 	path = get_path(envp, cmd_list[0]);
 	if (path == NULL)
 	{
